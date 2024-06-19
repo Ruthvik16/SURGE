c281073d05b8582465c0ee3324f19c635fc399c0
Ruthvik16 <121503571+Ruthvik16@users.noreply.github.com>
2024-06-19 17:44:41 +0530

    Update quantum_test.py

--- SURGE/quantum_test.py
+++ SURGE/quantum_test.py
@@ -1,11 +1,17 @@
 import qiskit
-# circuit = qiskit.QuantumCircuit(2)
-# circuit.h(0)
-# creg = qiskit.ClassicalRegister(2)
-# circuit.add_register(creg)
-# circuit.measure([0,1],[creg[0],creg[1]])
-# simulator = qiskit.Aer.get_backend('qasm_simulator')
-# result = qiskit.execute(circuit, backend=simulator).result()
-# count = result.get_counts()
-# print(count)
-print(qiskit.__qiskit_version__)
+from qiskit_aer import Aer
+from qiskit import transpile
+
+circuit = qiskit.QuantumCircuit(2)
+circuit.h(0)
+creg = qiskit.ClassicalRegister(2)
+circuit.add_register(creg)
+circuit.measure([0,1],[creg[0],creg[1]])
+
+simulator = Aer.get_backend('qasm_simulator')
+
+new_circuit = transpile(circuit, simulator)  
+job = simulator.run(new_circuit)
+result = job.result()
+count = result.get_counts()
+print(count)
